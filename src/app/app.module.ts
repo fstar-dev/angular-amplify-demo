34aa1cda24d1c4f37a2a890a1b8c72c980b1cc6e
fstar-dev <daigokenshi@gmail.com>
2021-04-14 09:52:43 -0400

    change views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
 
+/* new form imports */
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +13,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AmplifyUIAngularModule,
+    /* configuring form modules */
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
