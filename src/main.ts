d0658c6179a1c6ec1d52f18ef7167b9f28d9c370
dev0200@outlook.com <dev0200@outlook.com>
2021-05-12 01:17:06 -0500

    test 3

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-
+//this is to STV1 test git merge
+//this is to test git merge
+//this is to STV1 test git merge
 //this is to STV1 test git merge
 import { environment } from './environments/environment';
 
